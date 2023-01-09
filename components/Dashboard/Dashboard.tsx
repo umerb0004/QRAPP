6261a4ad4d1e47d0f560812cf72641c9fc1756aa
Adeel <100590488+adeelism1@users.noreply.github.com>
2023-01-09 20:49:23 +0500

    Rapp 3xpv3hj create UI of dashboard updated (#37)
    * Fixed double profile side bar and code refactor
    
    * replaced bootstrap and css with tailwind
    
    * fixed the button hover bug
    
    * removed lottie animation and lottie package
    
    * removed lottie and added gif instead
    
    * styling
    
    Co-authored-by: Fareed-Murtaza <fareed.murtaza@devsinc.com>

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,56 +1,53 @@
-import Lottie from 'react-lottie'
-
-import lottieFile from '../../public/welcome_bot.json'
-
 import styles from '@styles/Dashboard.module.css'
 
-const Dashboard = () => {
-  return <>
-    <section className='container py-5 pl-5 pr-1 bg-slate-50'>
-      <div className='card'>
-        <div className='col-md-12'>
-          <div className='flex align-items-center'>
-            <div className='col-md-5 pl-5 Lottie'>
-              <Lottie
-                height={300}
-                width={300}
-                options={{
-                  loop: true,
-                  animationData: lottieFile,
-                  autoplay: true,
-                }}
-              />
-            </div>
+import Image from 'next/image'
 
-            <div className='col-md-7'>
-              <h1>
-                <b>WELCOME</b>
-              </h1>
-              <p className={`${styles.paragraph} mt-1`}>Devsinc Family</p>
+const Dashboard = () => {
+  return (
+    <>
+      <section className='container py-5 pl-5 pr-1 bg-slate-50'>
+        <div className='card'>
+          <div className='col-md-12'>
+            <div className='flex align-items-center'>
+              <div className='col-md-5 pl-5 Lottie'>
+                <Image
+                  src='/welcome.gif'
+                  alt='Picture of the author'
+                  width={300}
+                  height={300}
+                />
+              </div>
+
+              <div className='col-md-7'>
+                <h1>
+                  <b>WELCOME</b>
+                </h1>
+                <p className={`${styles.paragraph} mt-1`}>Devsinc Family</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className='mt-4'>
-        <div className='flex justify-between'>
-          <button className='custom-card w-5/12'>
-            <h2>
-              <b className='mr-4'>10</b>
-              <span className={styles.pending}>Pending Reviews</span>
-              <i className='ml-5 icon fa-solid fa-pen-to-square' />
-            </h2>
-          </button>
-          <button className='custom-card w-5/12'>
-            <h2>
-              <b className='mr-4'>10</b>
-              <span className={styles.pending}>Pending Approval Reviews</span>
-              <i className='ml-5 fa-solid fa-ranking-star' />
-            </h2>
-          </button>
+        <div className='mt-4'>
+          <div className='pending-component flex justify-between'>
+            <button className='custom-card w-5/12'>
+              <h2>
+                <b className='mr-4'>10</b>
+                <span className={styles.pending}>Pending Reviews</span>
+                <i className='ml-5 icon fa-solid fa-pen-to-square' />
+              </h2>
+            </button>
+            <button className='custom-card w-5/12'>
+              <h2>
+                <b className='mr-4'>10</b>
+                <span className={styles.pending}>Pending Approval Reviews</span>
+                <i className='ml-5 fa-solid fa-ranking-star' />
+              </h2>
+            </button>
+          </div>
         </div>
-      </div>
-    </section>
-  </>
+      </section>
+    </>
+  )
 }
 
 export default Dashboard
